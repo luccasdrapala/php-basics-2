1b9308913c5a3a46be13bcae6d56a372278c9458
unknown <luccasdrapala@gmail.com>
2023-03-02 19:14:37 -0300

    building a validate

--- app/functions/validate.php
+++ app/functions/validate.php
@@ -1,5 +1,29 @@
 <?php 
 
 function validate(array $fields) {
-    
+
+    $validate = [];
+
+    foreach($fields as $field => $type){
+
+        switch($type) {
+
+        case 's':
+
+            $validate[$field] = filter_var($_POST[$field], FILTER_SANITIZE_STRING);
+            break;
+        
+        case 'e':
+
+            $validate[$field] = filter_var($_POST[$field], FILTER_SANITIZE_EMAIL);
+            break;
+        
+        case 'i': 
+
+            $validate[$field] = filter_var($_POST[$field], FILTER_SANITIZE_NUMBER_INT);
+            break;
+        }
+    }
+
+    return (object) $validate;
 }
--- public/pages/forms/form-contato.php
+++ public/pages/forms/form-contato.php
@@ -4,6 +4,13 @@ require '../../../bootstrap.php';
 
 echo 'Hello World cadastrar !!!<br>';
 
-dd($_POST);
+// seto os tipos do validae, vao cair no swith por tipo de dado
+
+$validade = validate([
+    'name' => 's',
+    'email' => 'e',
+    'subject' => 's',
+    'message' => 's' 
+])
 
 ?>
