0fa8bd0900f500f7e3cfb4d3ccd31122f023875a
unknown <luccasdrapala@gmail.com>
2023-03-07 21:06:59 -0300

    inserindo algumas funçoes no database

--- app/functions/database.php
+++ app/functions/database.php
@@ -2,9 +2,11 @@
 
 function connection(){
 
-    $pdo = new \PDO("mysql:host=localhost;dbname=users;charset=utf8;'root', 'root'");
+    $pdo = new \PDO("mysql:host=localhost;dbname=users;charset=utf8;'root', ''");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+
+    return $pdo;
 
 }
 
